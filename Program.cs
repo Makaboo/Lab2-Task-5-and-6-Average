6585a198388f42c9ab224591e7de1582fc961528
Makaboo <lxiao@kean.edu>
2023-02-06 13:53:40 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FindAvg
+{
+    public class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Xiao Li / Kanchan Khadka " +
+                "\nCPS 3330*01 - SOFTWARE DEV WITH FRAMEWORKS");
+            Console.WriteLine("Instructor: Dr. Yulia Kumar");
+            Console.WriteLine("Version: {0}", Environment.Version.ToString());
+
+            double a,b,c, avg;
+
+            Console.Write("\nEnter a interger number: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter a another interger number: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter a another interger number: ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            avg = TestAvg.Average.MyAvg1(a,b);
+            avg = TestAvg.Average.MyAvg1(a,b,c);
+        }
+
+    }
+}
+
