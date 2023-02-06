d9f9fbccb9783b6b073b3c05b4398539f2ba654b
Makaboo <123009074+Makaboo@users.noreply.github.com>
2023-02-06 14:01:46 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,23 @@
+﻿namespace TestAvg
+{
+    public class Average
+    {
+        public static double MyAvg1(double num1, double num2)
+        {
+            double avg;
+            avg = (num1 + num2) / 2;
+
+            Console.WriteLine("\nThe avgerage of " + num1 + " and " + num2 + " will return " + avg);
+            return avg;
+        }
+
+        public static double MyAvg1(double num1, double num2, double num3)
+        {
+            double avg;
+            avg = (num1 + num2 + num3) / 3;
+
+            Console.WriteLine("The avgerage of " + num1 + ", " + num2 + " and " + num3 + " will return " + avg);
+            return avg;
+        }
+    }
+}
